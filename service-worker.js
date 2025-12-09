689770f32d928769e67b97bf22acaf11f3269410
DrZaff <Zaffutobj@gmail.com>
2025-12-09 06:36:00 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = "clinicaltools-HS-v1"; // change APPKEY per app, e.g. "abg" or "aki"
+const CACHE_NAME = "clinicaltools-HS-v1";
 const ASSETS = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/manifest.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png",
-  "/icons/apple-touch-icon-180.png"
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon-180.png"
 ];
 
 self.addEventListener("install", event => {
@@ -33,12 +33,11 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(res => {
-      // If we have it cached, use it; otherwise fetch from network
       return (
         res ||
         fetch(event.request).catch(() => {
-          // If offline and request fails, fall back to index.html
-          return caches.match("/index.html");
+          // If offline and request fails, fall back to cached index.html
+          return caches.match("./index.html");
         })
       );
     })
